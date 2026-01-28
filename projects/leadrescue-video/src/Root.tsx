5fd13caf412b0036bad30840207442aa28ec8416
Rook <rook@clawd.bot>
2026-01-28 13:27:49 +0000

    feat: add promo video with voiceover for MissedCallCloser

--- projects/leadrescue-video/src/Root.tsx
+++ projects/leadrescue-video/src/Root.tsx
@@ -1,16 +1,34 @@
 import { Composition } from "remotion";
+import { PromoVideo } from "./PromoVideo";
 import { LeadRescueDemo } from "./LeadRescueDemo";
+import { MissedCallCloserVideo } from "./MissedCallCloser";
 
-export const RemotionRoot: React.FC = () => {
+export const RemotionRoot = () => {
   return (
     <>
       <Composition
+        id="PromoVideo"
+        component={PromoVideo}
+        durationInFrames={1080}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
         id="LeadRescueDemo"
         component={LeadRescueDemo}
-        durationInFrames={495}
+        durationInFrames={900}
         fps={30}
-        width={1080}
-        height={1920}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="MissedCallCloser"
+        component={MissedCallCloserVideo}
+        durationInFrames={450}
+        fps={30}
+        width={1920}
+        height={1080}
       />
     </>
   );
